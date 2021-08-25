f86a5618bd638c71873b1bfcf74b88bccd35ea27
GuiCelebrini <celebrinig@gmail.com>
2021-08-24 21:39:56 -0300

    Created model App

---
+++ app/src/main/java/com/android/guicelebrini/professoresvirtuais/model/App.java
@@ -0,0 +1,60 @@
+package com.android.guicelebrini.professoresvirtuais.model;
+
+public class App {
+    String name;
+    String developer;
+    String downloadLink;
+    String imagem;
+    String description;
+    String firebaseId;
+
+    public App(){}
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDeveloper() {
+        return developer;
+    }
+
+    public void setDeveloper(String developer) {
+        this.developer = developer;
+    }
+
+    public String getDownloadLink() {
+        return downloadLink;
+    }
+
+    public void setDownloadLink(String downloadLink) {
+        this.downloadLink = downloadLink;
+    }
+
+    public String getImagem() {
+        return imagem;
+    }
+
+    public void setImagem(String imagem) {
+        this.imagem = imagem;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getFirebaseId() {
+        return firebaseId;
+    }
+
+    public void setFirebaseId(String firebaseId) {
+        this.firebaseId = firebaseId;
+    }
+}
